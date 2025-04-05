dcef6bbb6d2e2172d0fd1a6fdce24a5b5c0bb987
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-04-05 16:54:44 +0200

    ### Added - integraded lwip raw API example: -> adapted PLL -> added MPU configs -> adapted linker script -> added caching support to example functions

--- app/app_ethernet.h
+++ app/app_ethernet.h
@@ -1,12 +1,12 @@
 /**
   ******************************************************************************
-  * @file    SPI/SPI_FullDuplex_ComDMA/CM7/Inc/main.h
+  * @file    LwIP/LwIP_TCP_Echo_Server/Inc/app_ethernet.h 
   * @author  MCD Application Team
-  * @brief   Header for main.c module
+  * @brief   Header for app_ethernet.c module
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2018 STMicroelectronics.
+  * Copyright (c) 2017 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -17,26 +17,41 @@
   */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __APP_ETHERNET_H
+#define __APP_ETHERNET_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32h7xx_hal.h"
-#include "stm32h7xx_nucleo.h"
+#include "lwip/netif.h"
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-/* User can use this section to tailor SPIx instance used and associated
-   resources */
-
+/* DHCP process states */
+#define DHCP_OFF                   (uint8_t) 0
+#define DHCP_START                 (uint8_t) 1
+#define DHCP_WAIT_ADDRESS          (uint8_t) 2
+#define DHCP_ADDRESS_ASSIGNED      (uint8_t) 3
+#define DHCP_TIMEOUT               (uint8_t) 4
+#define DHCP_LINK_DOWN             (uint8_t) 5
+   
 /* Exported macro ------------------------------------------------------------*/
-#define COUNTOF(__BUFFER__)   (sizeof(__BUFFER__) / sizeof(*(__BUFFER__)))
-
 /* Exported functions ------------------------------------------------------- */
+void ethernet_link_status_updated(struct netif *netif);
+void Ethernet_Link_Periodic_Handle(struct netif *netif);
+#if LWIP_DHCP
+void DHCP_Process(struct netif *netif);
+void DHCP_Periodic_Handle(struct netif *netif);
+#endif  
 
-#define WAIT_FOR_USER_BUTTON	(1U)
+#ifdef __cplusplus
+}
+#endif
 
-void Error_Handler(void);
+#endif /* __APP_ETHERNET_H */
 
-#endif /* __MAIN_H */
+
+
 
