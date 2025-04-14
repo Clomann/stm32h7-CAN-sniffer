04a3ef42b9f8b8825c42be0faf99a2d28cadd9fc
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-04-14 21:00:12 +0200

    added member "mode" to app config

--- app/FileHandler.h
+++ app/FileHandler.h
@@ -4,7 +4,9 @@
 
 typedef struct {
     uint32_t baudrate;
+    /* CAN operatig mode (1: normal, 2: listen only)*/
+    uint8_t mode; 
     uint8_t ip[4U];
 } AppConfigType;
 
-int FileHandler_LoadConfig(char *buff, uint32_t len, AppConfigType *cfg);
+int FileHandler_ParseConfig(char *buff, uint32_t len, AppConfigType *cfg);
