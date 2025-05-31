3256da00084af88fd3e4a56c6a60ee9c44986ac3
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-05-31 21:55:12 +0200

    ### Refactored - extended CommDriver type by opaque instance type for the driver to keep context - introduce "dev" arguemnt to all interface functions to pass opaque context around - prepared introduction of second channel

--- drivers/CanAbs/CanAbs.h
+++ drivers/CanAbs/CanAbs.h
@@ -3,13 +3,12 @@
 #include "stm32h7xx_hal.h"
 #include "CommManager.h"
 
-int CanAbs_Init(void);
-int CanAbs_Send(void);
-int CanAbs_Receive(FDCAN_ClassicFrame *frame);
-
-comm_status_t CanAbs_Start(void);
-comm_status_t CanAbs_Stop(void);
-comm_status_t CanAbs_SetBaudrate(uint32_t baudrate);
+comm_status_t CanAbs_Init_Can1(void);
+comm_status_t CanAbs_Send_Can1(void);
+comm_status_t CanAbs_Receive_Can1(FDCAN_ClassicFrame *frame);
+comm_status_t CanAbs_Start_Can1(void);
+comm_status_t CanAbs_Stop_Can1(void);
+comm_status_t CanAbs_SetBaudrate_Can1(uint32_t baudrate);
 
 comm_status_t fdcan_create_message_1(FDCAN_Message*, uint8_t*, uint32_t);
 comm_status_t fdcan_create_message_2(FDCAN_Message*, uint8_t*, uint32_t);
--- drivers/fdcan/fdcan_cfg.h
+++ drivers/fdcan/fdcan_cfg.h
@@ -5,30 +5,60 @@
  *      Author: Clemens
  */
 
+
+#define FDCAN_MAX_INSTANCES 2U
+
 /* User can use this section to tailor FDCANx instance used and associated
    resources */
-/* Definition for FDCANx clock resources */
-#define FDCANx                      FDCAN1
-#define FDCANx_CLK_ENABLE()         __HAL_RCC_FDCAN_CLK_ENABLE()
-#define FDCANx_RX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOH_CLK_ENABLE()
-#define FDCANx_TX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOH_CLK_ENABLE()
 
-#define FDCANx_FORCE_RESET()   __HAL_RCC_FDCAN_FORCE_RESET()
-#define FDCANx_RELEASE_RESET() __HAL_RCC_FDCAN_RELEASE_RESET()
+/* Definition for FDCANx clock resources */
+#define FDCANx_CLK_ENABLE()     __HAL_RCC_FDCAN_CLK_ENABLE()
+#define FDCANx_FORCE_RESET()    __HAL_RCC_FDCAN_FORCE_RESET()
+#define FDCANx_RELEASE_RESET()  __HAL_RCC_FDCAN_RELEASE_RESET()
+
+
+/* FDCAN 1 ========================================= */
+#define FDCAN_1             FDCAN1
+
+#define FDCAN1_RX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
+#define FDCAN1_TX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
 
 /*!< timestamp resolution in micro seconds */
-#define FDCANx_TIMESTAMP_RESOLUTION     (1U)
+#define FDCAN1_TIMESTAMP_RESOLUTION     (1U)
 
-/* Definition for FDCANx Pins */
-#define FDCANx_TX_PIN       GPIO_PIN_9
-#define FDCANx_TX_GPIO_PORT GPIOB
-#define FDCANx_TX_AF        GPIO_AF9_FDCAN1
-#define FDCANx_RX_PIN       GPIO_PIN_8
-#define FDCANx_RX_GPIO_PORT GPIOB
-#define FDCANx_RX_AF        GPIO_AF9_FDCAN1
+/* Definition for FDCAN1 Pins */
+#define FDCAN1_TX_PIN       GPIO_PIN_9
+#define FDCAN1_TX_GPIO_PORT GPIOB
+#define FDCAN1_TX_AF        GPIO_AF9_FDCAN1
+#define FDCAN1_RX_PIN       GPIO_PIN_8
+#define FDCAN1_RX_GPIO_PORT GPIOB
+#define FDCAN1_RX_AF        GPIO_AF9_FDCAN1
 
-/* Definition for FDCANx's NVIC IRQ and IRQ Handlers */
-#define FDCANx_IRQn       FDCAN1_IT0_IRQn
-#define FDCANx_IRQHandler FDCAN1_IT0_IRQHandler
+/* Definition for FDCAN1's NVIC IRQ and IRQ Handlers */
+#define FDCAN1_IRQn       FDCAN1_IT0_IRQn
+#define FDCAN_1_IRQHandler FDCAN1_IT0_IRQHandler
 
-#define FDCANx_MAX_MSG_COUNT 10u
+#define FDCAN1_MAX_MSG_COUNT 10u
+
+/* FDCAN 2 ========================================= */
+#define FDCAN_2             FDCAN2
+
+#define FDCAN2_RX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
+#define FDCAN2_TX_GPIO_CLK_ENABLE() __HAL_RCC_GPIOB_CLK_ENABLE()
+
+/*!< timestamp resolution in micro seconds */
+#define FDCAN2_TIMESTAMP_RESOLUTION     (1U)
+
+/* Definition for FDCAN2 Pins */
+#define FDCAN2_TX_PIN       GPIO_PIN_6
+#define FDCAN2_TX_GPIO_PORT GPIOB
+#define FDCAN2_TX_AF        GPIO_AF9_FDCAN2
+#define FDCAN2_RX_PIN       GPIO_PIN_12
+#define FDCAN2_RX_GPIO_PORT GPIOB
+#define FDCAN2_RX_AF        GPIO_AF9_FDCAN2
+
+/* Definition for FDCAN2's NVIC IRQ and IRQ Handlers */
+#define FDCAN2_IRQn       FDCAN2_IT0_IRQn
+#define FDCAN_2_IRQHandler FDCAN2_IT0_IRQHandler
+
+#define FDCAN2_MAX_MSG_COUNT 10u
