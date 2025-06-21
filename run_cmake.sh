8892190d25b7349ef15f501e1868021345ba9858
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-06-21 19:31:12 +0200

    ### Added - fetching freertos kernel through cmake
    ### Fixed
    - pushed arch/ cpu flags to targets lwrb and freertos

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,5 +1,10 @@
-rm -rf ./build
+# rm -rf ./build
 mkdir -p ./build
+
+git -C libs/STM32_HAL/STM32H7 submodule update --recursive --init Drivers/BSP/STM32H7xx_Nucleo
+git -C libs/STM32_HAL/STM32H7 submodule update --recursive --init Drivers/BSP/Components/lan8742
+git -C libs/STM32_HAL/STM32H7 submodule update --recursive --init Drivers/CMSIS/Device/ST/STM32H7xx
+git -C libs/STM32_HAL/STM32H7 submodule update --recursive --init Drivers/BSP/Components/Common
 
 if [ -d build ]; then
     cmake --preset "Debug" -B ./build/
