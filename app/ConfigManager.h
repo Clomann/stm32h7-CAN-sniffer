6949d1b0a9ac17d3a93fdde1b79e09920dcd5a73
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-08-31 16:56:13 +0200

    refactor: moved typedefs into dedicated files for CanLogManager, ConfigManager and FileHandler

--- app/ConfigManager.h
+++ app/ConfigManager.h
@@ -4,25 +4,11 @@
 #include <stdbool.h>
 
 #include "FileHandler.h"
+#include "ConfigManagerTypes.h"
 
 #define DEFAULT_CONFIG_FILENAME "CONF.TXT"
 #define MAX_FILENAME_LENGTH     255
 #define CONFIG_BUFFER_SIZE      512
-
-typedef struct ConfigManager ConfigManagerType;
-
-typedef enum
-{
-    CONFIG_OK = 0,
-    CONFIG_NOT_OK,
-    CONFIG_ERROR_MOUNT_FAILED,
-    CONFIG_ERROR_FILE_OPEN,
-    CONFIG_ERROR_FILE_READ,
-    CONFIG_ERROR_FILE_WRITE,
-    CONFIG_ERROR_INVALID_PARAM,
-    CONFIG_ERROR_NOT_INITIALIZED,
-    CONFIG_ERROR_PARSE_FAILED
-} ConfigResultType;
 
 struct ConfigManager
 {
@@ -40,15 +26,6 @@ struct ConfigManager
     ConfigResultType lastError;
     char buffer[CONFIG_BUFFER_SIZE];
 };
-
-typedef struct
-{
-    char filename[256];
-    uint32_t size;
-    uint32_t lastModified;
-    bool isOpen;
-    bool needsSync;
-} ConfigFileInfoType;
 
 void ConfigManager_Init(
     ConfigManagerType *manager,
