7648c0fc826bf1fd3e1985fba794b688b9f736df
Clemens Bromann <88851762+Clomann@users.noreply.github.com>
2025-12-26 15:20:41 +0100

    fix(fdcan): increase interrupt watermark to reduce time btween ISR calls at worst case frame rate

--- drivers/fdcan/fdcan_cfg.h
+++ drivers/fdcan/fdcan_cfg.h
@@ -14,7 +14,7 @@
 #define FDCAN_RAM_RX_ELEMENTS       (64U)
 #define FDCAN_RAM_TX_ELEMENTS       (4U)
 #define FDCAN_IRQ_NOTIFICATION      FDCAN_IT_RX_FIFO0_WATERMARK
-#define FDCAN_IRQ_RX_WATERMARK      (16U)
+#define FDCAN_IRQ_RX_WATERMARK      (54U)
 
 /* User can use this section to tailor FDCANx instance used and associated
    resources */
